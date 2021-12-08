622dda44214167e3ffdb1268a9b776e43e1f4d46
ATE47 <wilate@yahoo.fr>
2021-12-08 22:20:33 +0100

    secu add salt param

--- security/sha3.go
+++ security/sha3.go
@@ -7,19 +7,19 @@ import (
 )
 
 func usage() {
-	fmt.Fprintf(os.Stderr, "usage: %s [f [file]|s [str]]\n  f - file\n  s - string\n", os.Args[0])
+	fmt.Fprintf(os.Stderr, "usage: %s [f [file]|s [str]] [salt]\n  f - file\n  s - string\n", os.Args[0])
 	os.Exit(-1)
 }
 
 // sha3 of a byte array
 // (here is where the fun begins)
-func sha3(text []byte) []byte {
+func sha3(text []byte, salt []byte) []byte {
 
 	return text
 }
 
 // sha3 of a file
-func sha3f(filename string) []byte {
+func sha3f(filename string, salt []byte) []byte {
 	bytes, err := os.ReadFile(filename)
 
 	if err != nil {
@@ -27,27 +27,27 @@ func sha3f(filename string) []byte {
 		os.Exit(-1)
 	}
 
-	return sha3(bytes)
+	return sha3(bytes, salt)
 }
 
 // sha3 of a string
-func sha3s(text string) []byte {
-	return sha3([]byte(text))
+func sha3s(text string, salt []byte) []byte {
+	return sha3([]byte(text), salt)
 }
 
 func main() {
-	if len(os.Args) < 3 {
+	if len(os.Args) < 4 {
 		usage()
 	}
 
 	function := os.Args[1]
 
 	switch function {
-	case "f":
-		fmt.Println(hex.EncodeToString(sha3f(os.Args[2])))
+	case "f", "F":
+		fmt.Println(hex.EncodeToString(sha3f(os.Args[2], []byte(os.Args[3]))))
 		break
-	case "s":
-		fmt.Println(hex.EncodeToString(sha3s(os.Args[2])))
+	case "s", "S":
+		fmt.Println(hex.EncodeToString(sha3s(os.Args[2], []byte(os.Args[3]))))
 		break
 	default:
 		usage()
